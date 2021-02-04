abaa7d6b1c040b9ef56ea3f4dfc28ae940562eb5
Sławomir Śledź <slawomir.sledz@dsstream.com>
2021-02-04 17:48:46 +0100

    + data structures

---
+++ src/main/scala/basics/datastructures.scala
@@ -0,0 +1,10 @@
+package basics
+
+/**
+ *   - algebraic data structures (product, sum)
+ *     - Maybe
+ *     - List
+ */
+object datastructures extends App {
+
+}
--- src/main/scala/basics/functions.scala
+++ src/main/scala/basics/functions.scala
@@ -2,18 +2,25 @@ package basics
 
 /**
   *  - function definition
+  *  - closure
   *  - function application
   *  - function composition
   *  - pure function
+  *  - side effects
   *  - referentially transparency
   *  - function as a value
+  *  - point free style
+  *  - recursion (tail call, trampoline)
   *  - function literals
   *  - higher order functions
   *  - parameters list
   *  - variadic functions
   *  - currying
   *  - partial application
-  *  - partial function => total Function
+  *  - partial function -> total Function
   *  - parametric, adhoc and subtyping polymorphism (https://medium.com/@sinisalouc/ad-hoc-polymorphism-and-type-classes-442ae22e5342)
+ *   - strict vs non-strict functions
   */
-object functions extends App {}
+object functions extends App {
+
+}
