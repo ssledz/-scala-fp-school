9db12b513fc8a6a0ba05c86a569910e369aaa406
Sławomir Śledź <slawomir.sledz@dsstream.com>
2021-02-10 22:26:44 +0100

    session 3

--- src/main/scala/basics/homework3.scala
+++ src/main/scala/basics/homework3.scala
@@ -1,11 +1,15 @@
 package basics
 
 /**
-  *  - https://stackoverflow.com/questions/17270003/why-are-classes-inside-scala-package-objects-dispreferred
-  *  - https://www.youtube.com/watch?v=V10hzjgoklA&list=PLw1ByaFQI-9ICX6a6T4f2OW-w9SV17MUg&index=1&t=1609s&ab_channel=LambdaWorld
-  *  - https://www.youtube.com/watch?v=yjmKMhJOJos&ab_channel=YOW%21Conferences
-  *  - https://docs.scala-lang.org/tutorials/FAQ/finding-implicits.html
-  *  - https://github.com/typelevel/simulacrum
- *   - https://github.com/ssledz/fp-toy
+  * Read:
+  *   - https://stackoverflow.com/questions/17270003/why-are-classes-inside-scala-package-objects-dispreferred
+  *   - https://docs.scala-lang.org/tutorials/FAQ/finding-implicits.html
+  *   - https://github.com/typelevel/simulacrum
+  *   - https://github.com/ssledz/fp-toy
+  *
+  *  Watch:
+  *   - https://www.youtube.com/watch?v=V10hzjgoklA&list=PLw1ByaFQI-9ICX6a6T4f2OW-w9SV17MUg&index=1&t=1609s&ab_channel=LambdaWorld
+  *   - https://www.youtube.com/watch?v=yjmKMhJOJos&ab_channel=YOW%21Conferences
+  *
   */
 object homework3 extends App {}
