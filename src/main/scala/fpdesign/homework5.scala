1bf8ba70d618c195a9206d40b13de5b40b98cc91
Sławomir Śledź <slawomir.sledz@dsstream.com>
2021-03-02 11:47:36 +0100

    homework 5

--- src/main/scala/fpdesign/homework5.scala
+++ src/main/scala/fpdesign/homework5.scala
@@ -9,14 +9,19 @@ import fpdesign.functors.Functor
   *    - scala with cats book (read 1, 2, 3 chapters)
   *    - https://www.scala-exercises.org/cats/ (functor, monoid, semigroup)
   *    - implement
-  *      - map3, map4
+  *      - map, map2, map3, map4
   */
 object homework5 extends App {
 
   trait Applicative[F[_]] extends Functor[F] {
 
     def ap[A, B](fab: F[A => B])(fa: F[A]): F[B]
+
     def pure[A](a: A): F[A]
+
+    def map[A, B](fa: F[A])(f: A => B): F[B] = ???
+
+    def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = ???
 
     def map3[A, B, C, D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D): F[D] = ???
 
